faac892247b3a4c4980da89ef7c0c5885e579f09
fadjar <dark.fir21@gmail.com>
2019-03-13 20:17:27 +0700

    penambahan fungsi switch

--- kalkulator/kalkulator/Program.cs
+++ kalkulator/kalkulator/Program.cs
@@ -8,38 +8,82 @@ namespace kalkulator
         {
             Console.Title = "Aplikasi Kalkulator";
 
-            Console.Write("Inputkan Nilai a = ");
-            int a = int.Parse(Console.ReadLine()); // proses casting
+            double bil1, bil2, pilih;
 
-            Console.Write("Inputkan nilai b = ");
-            int b = int.Parse(Console.ReadLine()); // proses casting
 
-            Console.WriteLine(); // tambahkan baris kosong
+            Console.WriteLine("Silahkan Pilih Menu Di bawah ini : ");
+            Console.WriteLine();
+            Console.WriteLine("1. Penambahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+            Console.WriteLine();
+            Console.Write("Masukkan Pilihan : ");
+            pilih = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
 
-            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
-            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            switch(pilih)
+            {
 
-            Console.ReadKey();
+                case 1:
+            
+                Console.Write("Masukkan nilai a = ");
+                bil1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Masukkan nilai b = ");
+                bil2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Hasilnya adalah {0} + {1} = {2}", bil1, bil2, tambah(bil1, bil2));
+                    break;
+
+                case 2:
+            
+                Console.Write("Masukkan nilai a = ");
+                bil1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Masukkan nilai b = ");
+                bil2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Hasilnya adalah {0} - {1} = {2}", bil1, bil2, kurang(bil1, bil2));
+                    break;
+
+                case 3:
+            
+                Console.Write("Masukkan nilai a = ");
+                bil1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Masukkan nilai b = ");
+                bil2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Hasilnya adalah {0} * {1} = {2}", bil1, bil2, kali(bil1, bil2));
+                    break;
+
+                case 4:
+            
+                Console.Write("Masukkan nilai a = ");
+                bil1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Masukkan nilai b = ");
+                bil2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Hasilnya adalah {0} / {1} = {2}", bil1, bil2, bagi(bil1, bil2));
+                    break;
+
+                default:
+                    Console.WriteLine("input tidak valid");
+                    break;
+            }
+           
         }
 
-        static int Penambahan(int a, int b)
+        static double tambah(double a, double b)
         {
             return a + b;
         }
 
-        static int Pengurangan(int a, int b)
+        static double kurang(double a, double b)
         {
             return a - b;
         }
 
-        static int Perkalian(int a, int b)
+        static double kali(double a, double b)
         {
             return a * b;
         }
 
-        static int Pembagian(int a, int b)
+        static double bagi(double a, double b)
         {
             return a / b;
         }
